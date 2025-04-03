15080141c9290ccc0eed4604eb5c8043fe9a0c8b
Immanuel <immanuelsundar07#gmail.com>
2025-04-03 12:59:46 +0000

    Updated sh script

--- simple.sh
+++ simple.sh
@@ -3,5 +3,5 @@
 #Author: Immanuel 
 #Task: running
 
-python3 -m http.server 8080
-
+python3 -m http.server 8080 &  # Run in the background
+sleep 10  # Keep it alive for 10 seconds (adjust as needed)
